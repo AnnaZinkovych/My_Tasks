1f61d64edd7e5d99d0bd489e78a06ed1deaaced3
AnnaZinkovych <162343249+AnnaZinkovych@users.noreply.github.com>
2024-03-16 16:44:51 +0000

    Add comments sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,31 +6,31 @@ from spacytextblob.spacytextblob import SpacyTextBlob
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('spacytextblob') 
 
-def preprocess(text):  #preprocessing function
+def preprocess(text):  #defining preprocessing function
   doc = nlp(text.lower().strip())
   return ' '.join([token.lemma_.lower() for token in doc if not token.is_stop and not token.is_punct])
 
-def analyze_polarity(text):
+def analyze_polarity(text):# Defining analyze polarity function
   doc = nlp(text)
   blob = TextBlob(text)
   polarity = blob.sentiment.polarity
   return polarity
 
-def spacy_polarity(text):
+def spacy_polarity(text): # Defining one more analyze polarity function
 
     doc = nlp(text)
     rep = doc
     polarity_value = doc._.blob.polarity
     return polarity_value
 
-my_data = pd.read_csv("amazon_product_reviews.csv", sep =",", low_memory=False)
-reviews_list = my_data[["id", "name", "reviews.text", "reviews.title"]]
-test_review = reviews_list.iloc[0:5, : ]
-test_review.dropna(inplace=True, axis=0)
-test_review["processed_reviews"] = test_review["reviews.text"].apply(preprocess)
+my_data = pd.read_csv("amazon_product_reviews.csv", sep =",", low_memory=False) #reading dataset
+reviews_list = my_data[["id", "name", "reviews.text", "reviews.title"]] #defining which column we are going to use
+test_review = reviews_list.iloc[0:5, : ] #Taking only first 5 rows to work with
+test_review.dropna(inplace=True, axis=0) #clening data
+test_review["processed_reviews"] = test_review["reviews.text"].apply(preprocess) #applying prprocessing function
 data = test_review['processed_reviews']
 analysis = {}
-for i in range(len(data)):
+for i in range(len(data)): #analyzing polarity for our test dataset
     my_sentence = data[i]
     print(my_sentence)
     val = spacy_polarity(my_sentence)
